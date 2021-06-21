79ccda052265f4a7244e81e39d66b6978d12b758
venkat143-v <81428675+venkat143-v@users.noreply.github.com>
2021-06-21 10:20:59 +0530

    Add files via upload

---
+++ Armstrong_0_1000.java
@@ -0,0 +1,32 @@
+import java.util.*;
+public class Armstrong_0_1000 {
+	public static void main(String[] args) {			
+		int d=0;
+		int sum =0;
+		for(int n =0 ; n <= 1000; n++)
+		{
+			int t = n; // 
+		// no of digits
+		while(n > 0)
+		{
+			int rem = n%10;
+			d++; // d = 3
+			n= n/10;
+		} // end of while loop
+		n = t; 
+		// check Armstrong
+		while(n>0)
+		{
+			int rem = n%10; // rem = 153%10 = 3,5,1
+			sum = sum + (int)Math.pow(rem, d);
+			n = n/10;
+		}	// end of while loop
+		// verify Armstrong
+		if(t == sum )
+		  {	System.out.println(t); }
+		
+		d = 0;
+		sum = 0;
+		n = t;
+		}// end of for loop
+} }
